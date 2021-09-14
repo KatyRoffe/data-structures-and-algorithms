42ca7807ee523234ce63a8d1bb231fcbe21165ab
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-09-13 20:27:18 -0700

    Q6 complete

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -117,7 +117,14 @@ This function should use forEach to populate your grocery list based on the stor
 ------------------------------------------------------------------------------------------------ */
 
 const createList = (availableItems) => {
-  // Solution code here...
+  const fruitArray = [];
+
+  availableItems.forEach(item =>{
+    if (item.available){
+      fruitArray.push(item.name);
+    }
+  });
+  return fruitArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -180,7 +187,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   const inventory = [{ name: 'apples', available: true }, { name: 'pears', available: true }, { name: 'oranges', available: false }, { name: 'bananas', available: true }, { name: 'blueberries', available: false }];
 
   test('It should only add the available items to the list', () => {
