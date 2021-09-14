1699dd352f006e99bad18406ccb484cfd3cf955e
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-09-13 20:41:23 -0700

    Even got Q7 done, yay!

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -142,7 +142,22 @@ Return the resulting output array.
 ------------------------------------------------------------------------------------------------ */
 
 const fizzbuzz = (arr) => {
-  // Solution code here...
+  const fizzyArray = [];
+  arr.forEach(number => {
+    if (number % 3 === 0 && number % 5 !== 0){
+      fizzyArray.push('Fizz');
+    } 
+    else if (number % 5 === 0 && number % 3 !== 0){
+      fizzyArray.push('Buzz');
+    } 
+    else if (number % 5 === 0 && number % 3 === 0){
+      fizzyArray.push('Fizz Buzz');
+    }
+    else {
+      fizzyArray.push(number)
+    }
+  })
+  return fizzyArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -196,7 +211,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   const inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
   test('It should print out messages or numbers', () => {
