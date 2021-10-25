cb835bd9a169930cbeaf33675d7e747ec7cb3b34
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-10-24 23:45:55 -0700

    Initial attempt at linked-list challenge.

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,30 @@
 'use strict';
 
 // Require our linked list implementation
-const LinkedLilst = require('../index');
+const LinkedList = require('../index');
 
-describe('Linked List', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+describe('This is to test a linked list', () => {
+  xit('Can successfully instantiate an empty linked list', () => {
+    const list = new LinkedList();
+    console.log(list);
+    expect(list.head).toBe(null);
+  });
+  xit('can properly insert into the linked list', () => {
+    const list = new LinkedList();
+    console.log(list);
+    list.insert(27);
+    expect(list.head.value).toBe(27);
+  });
+
+  xit('head property will properly point to the first node in the linked list', () => {
+    const list = new LinkedList();
+    console.log(list);
+    expect(list.head).toEqual(null);
+  });
+  xit('can properly insert multiple nodes into the linked list', () => {
+    const list = new LinkedList();
+    console.log(list);
+    list.insert('awesome possum', 6);
+    expect(list.head.value).toBe('awesome possum', 6);
   });
 });
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,66 @@
 'use strict';
 
-class LinkedList {
-
+//sets up the node
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
-module.exports = LinkedList;
+//sets up a linked list
+class LinkedList {
+  constructor() {
+    this.head = null;
+    //head is the very first node in the list
+  }
+
+  //begins traversing the list - going from node to node
+  traverse() {
+    let currentNode = this.head;
+    while(currentNode) {
+      console.log(currentNode.value);
+      currentNode = currentNode.next;
+    }
+  }
+  //inserts a new node into the list
+  insert(value) {
+    let currentNode = this.head;
+    let newNode = new Node(value);
+
+    if(!currentNode) {
+      this.head = newNode;
+    } else {
+      newNode.next = currentNode;
+      this.head= newNode;
+    }
+  }
+  includes(value) {
+    let currentNode = this.head;
+    while (currentNode) {
+      if(value === currentNode.value) {
+        return true;
+      } else if(currentNode.next === null) {
+        return false;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+  }
+  toString() {
+    let currentNode = this.head;
+    let string = '';
+
+    while(currentNode) {
+      string += `${currentNode.value} ->`;
+      currentNode = currentNode.next;
+    }
+    string += 'null';
+    return string;
+  }
+}
+
+
+
+
+module.exports = {LinkedList, Node};
