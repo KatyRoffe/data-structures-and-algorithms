342b92375a3971666b879ac27ac28b8b5ce5f830
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-10-25 20:02:24 -0700

    Adds new methods of append, insertBefore, and insertAfter.

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -35,6 +35,50 @@ class LinkedList {
       this.head= newNode;
     }
   }
+  //takes in two arguments and inserts a new value before an estabished value
+  insertBefore(value, newValue) {
+    let currentNode = this.head;
+    let newNode = new Node(newValue);
+
+    while(currentNode.value !== null) {
+      if(currentNode.value === value) {
+        newNode.next = currentNode;
+        this.head = newNode;
+      } else if(currentNode.next.value === value) {
+        let insertNode = currentNode.next;
+        currentNode.next = newNode;
+        newNode.next = insertNode;
+        return;
+      }
+      currentNode = currentNode.next;
+    }
+  }
+  // takes in two arguments and inserts a new value after an established value
+  insertAfter(value, newvalue) {
+    let currentNode = this.head;
+
+    while(currentNode) {
+      if(currentNode.value === value) {
+        let newNode = new Node(newvalue);
+        let insertNode = currentNode.next;
+        currentNode.next = newNode;
+        newNode.next = insertNode;
+      }
+      currentNode = currentNode.next;
+    }
+  }
+  //append a node at the end of the list and before null
+  append(value) {
+    let current = this.head;
+    while (current) {
+      if (current.next === null) {
+        current.next = new Node(value);
+        return;
+      }
+      current = current.next;
+    }
+  }
+
   includes(value) {
     let currentNode = this.head;
     while (currentNode) {
@@ -47,20 +91,29 @@ class LinkedList {
       }
     }
   }
+  //this takes in no arguments, but returns a string to reprent all the values in the list
   toString() {
     let currentNode = this.head;
     let string = '';
 
     while(currentNode) {
-      string += `${currentNode.value} ->`;
+      let listValues = currentNode.value;
       currentNode = currentNode.next;
+      string += `{${listValues}} ->`;
     }
-    string += 'null';
+    string += '{null}';
     return string;
   }
 }
 
+const linkedList = new LinkedList();
 
-
+linkedList.head = new Node(0);
+linkedList.append('apple');
+linkedList.append('orange');
+linkedList.append('banana');
+linkedList.insertBefore('orange', 'tomato' );
+linkedList.insertAfter('orange', 'carrot');
+linkedList.toString();
 
 module.exports = {LinkedList, Node};
