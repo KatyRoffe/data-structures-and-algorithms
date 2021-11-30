dabad46d47bc1a30e47e5787110c374524b48082
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-11-29 23:46:50 -0800

    Adds quick sort challenge and testing.

---
+++ javascript/401-challenges/quick-sort/quicksort.js
@@ -0,0 +1,44 @@
+'use strict';
+
+function quickSort(arr, left, right) {
+
+  if (left < right) {
+    let position = partition(arr, left, right);
+    if (left < position - 1) {
+      quickSort(arr, left, position - 1);
+    }
+    if (position < right) {
+      quickSort(arr, position, right);
+    }
+  }
+  return arr;
+}
+
+function partition(arr, left, right) {
+  let pivot = arr[Math.floor((right + left) / 2)],
+    i = left,
+    j = right;
+
+  while (i <= j) {
+    while (arr[i] < pivot) {
+      i += 1;
+    }
+    while (arr[j] > pivot) {
+      j -= 1;
+    }
+    if (i <= j) {
+      swap(arr, i, j);
+      i += 1;
+      j -= 1;
+    }
+  }
+  return i;
+}
+
+function swap(arr, left, right) {
+  let temp = arr[left];
+  arr[left] = arr[right];
+  arr[right] = temp;
+}
+
+module.exports = quickSort;
